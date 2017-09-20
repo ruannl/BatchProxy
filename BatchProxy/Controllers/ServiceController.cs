f90e9bacfbffd1ad5f186e408179dad17bf2e500
Ruann <ruann.linde@gmail.com>
2017-09-20 11:09:52 +0200

    Update ServiceController.cs

--- BatchProxy/Controllers/ServiceController.cs
+++ BatchProxy/Controllers/ServiceController.cs
@@ -40,5 +40,48 @@ namespace BatchProxy.Controllers
             }, JsonRequestBehavior.AllowGet);
         }
         
+        private static void GetJobInfo(string source, string destination)
+        {
+            if (!Directory.Exists(source)) return;
+
+            var sourceRootFolder = source.Substring(source.LastIndexOf('\\'), source.Length - source.LastIndexOf('\\'));
+            destination = destination + sourceRootFolder;
+
+            if (!Directory.Exists(destination))
+            {
+                Directory.CreateDirectory(destination);
+            }
+
+            var sourceFileSystemEntries = Directory.GetFileSystemEntries(source, "*", SearchOption.AllDirectories)
+                                                   .Select(path => path.Replace(source, ""))
+                                                   .ToArray();
+
+            var destinationFileSystemEntries = Directory.GetFileSystemEntries(destination, "*", SearchOption.AllDirectories)
+                                                        .Select(path => path.Replace(destination, ""))
+                                                        .ToArray();
+
+            var newDestinationFiles = sourceFileSystemEntries.Union(destinationFileSystemEntries)
+                                                             .Except(sourceFileSystemEntries.Intersect(destinationFileSystemEntries));
+
+            var existingDestinationFiles = sourceFileSystemEntries.Intersect(destinationFileSystemEntries);
+
+            var itemsToCopy = (from item in existingDestinationFiles
+                               let sourceFileName = string.Format("{0}{1}", source, item)
+                               let destinationFileName = string.Format("{0}{1}", destination, item)
+                               let sourceFileInfo = new FileInfo(sourceFileName)
+                               let destinationFileInfo = new FileInfo(destinationFileName)
+                               where !Equals(sourceFileInfo.Length, destinationFileInfo.Length) || 
+                                     !Equals(sourceFileInfo.LastWriteTime, destinationFileInfo.LastWriteTime)
+                               select sourceFileInfo.FullName).ToList();
+
+            Console.WriteLine("items to add [{0}]", newDestinationFiles.Count());
+            Console.WriteLine("items to copy [{0}]", itemsToCopy.Count());
+
+            foreach (var item in itemsToCopy)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        
     }
 }
